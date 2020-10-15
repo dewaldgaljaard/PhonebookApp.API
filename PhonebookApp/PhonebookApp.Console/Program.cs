3ce7c2e900a46818ee73e740dde594c301211c6f
dewald <dewald.galjaard@liberty.co.za>
2020-10-15 23:30:14 +0200

    Added  DbContext and migration tools to Console (must  go with executable)

--- PhonebookApp/PhonebookApp.Console/Program.cs
+++ PhonebookApp/PhonebookApp.Console/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace PhonebookApp.Console
+﻿namespace PhonebookApp.Console
 {
     class Program
     {
---
+++ PhonebookApp/PhonebookApp.Data/PhonebookAppContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using PhonebookApp.Domain;
+
+namespace PhonebookApp.Data
+{
+    public class PhonebookAppContext : DbContext
+    {
+        public DbSet<PhoneBook> Type { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // IMPORTANT: THIS IS NOT PRODUCTION READY. ONLY FOR DEMO PURPOSES.
+            optionsBuilder.UseSqlite("Filename=PhonebookAppDb.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+        }
+    }
+}
