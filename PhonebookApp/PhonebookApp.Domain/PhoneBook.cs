5d620b2ab949b0b2e353964275cf80946946ba8d
dewald <dewald.galjaard@liberty.co.za>
2020-10-15 22:54:01 +0200

    Updated constructor to ensure Entries is always instantiated

--- PhonebookApp/PhonebookApp.Domain/PhoneBook.cs
+++ PhonebookApp/PhonebookApp.Domain/PhoneBook.cs
@@ -6,5 +6,10 @@ namespace PhonebookApp.Domain
     {
         public string Name { get; set; }
         public List<Entry> Entries { get; set; }
+
+        public PhoneBook()
+        {
+            Entries = new List<Entry>();
+        }
     }
 }
